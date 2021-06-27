b4f2417885c9934a1d5be1a12df28ebac94b46c4
kspace-trk <keigonakao.0825@gmail.com>
2021-06-28 02:08:17 +0900

    [add]音数決定

--- initial_generation.js
+++ initial_generation.js
@@ -1,12 +1,10 @@
 const fs = require("fs");
-let one_measure_repeating_notes = []; // 1小節ごとの繰り返しnoteデータ
+let one_measure_repeating_notes = {}; // 1小節ごとの繰り返しnoteデータ
 let two_measure_repeating_notes = []; // 2小節ごとの繰り返しnoteデータ
 let four_measure_repeating_notes = []; // 4小節ごとの繰り返しnoteデータ
 const input_notes = () => {
-  one_measure_repeating_notes.push(
-    JSON.parse(
-      fs.readFileSync("./notes/one_measure_repeating_notes.json", "utf8")
-    )
+  one_measure_repeating_notes = JSON.parse(
+    fs.readFileSync("./notes/one_measure_repeating_notes.json", "utf8")
   );
   two_measure_repeating_notes.push(
     JSON.parse(
@@ -19,7 +17,17 @@ const input_notes = () => {
     )
   );
 };
+const calc_notes_num = () => {
+  let notes_nums = [];
+  one_measure_repeating_notes.notes.forEach((elem_song) => {
+    elem_song.forEach((elem_notes) => {
+      notes_nums.push(elem_notes.name.length);
+    });
+  });
+  return notes_nums[Math.floor(Math.random() * notes_nums.length)];
+};
 const main = () => {
   input_notes();
+  console.log("音数：" + calc_notes_num());
 };
 main();
