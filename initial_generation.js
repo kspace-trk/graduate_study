3984bf47853f90a9866f22d14a27243d67c2ed84
kspace-trk <keigonakao.0825@gmail.com>
2021-06-28 19:17:41 +0900

    [add]リズムの初期生成完成

--- initial_generation.js
+++ initial_generation.js
@@ -1,61 +1,79 @@
 const fs = require("fs");
+let measure_repeating_time = null; //1小節ごとの繰り返しは0, 2小節ごとの繰り返しは1, 4小節ごとの繰り返しは2
 let one_measure_repeating_notes = {}; // 1小節ごとの繰り返しnoteデータ
-let two_measure_repeating_notes = []; // 2小節ごとの繰り返しnoteデータ
-let four_measure_repeating_notes = []; // 4小節ごとの繰り返しnoteデータ
+let two_measure_repeating_notes = {}; // 2小節ごとの繰り返しnoteデータ
+let four_measure_repeating_notes = {}; // 4小節ごとの繰り返しnoteデータ
+const input_notes_data = [
+  one_measure_repeating_notes,
+  two_measure_repeating_notes,
+  four_measure_repeating_notes,
+];
 const times = []; //生成したリズムデータ
 const input_notes = () => {
-  one_measure_repeating_notes = JSON.parse(
+  input_notes_data[0] = JSON.parse(
     fs.readFileSync("./notes/one_measure_repeating_notes.json", "utf8")
   );
-  two_measure_repeating_notes.push(
-    JSON.parse(
-      fs.readFileSync("./notes/two_measure_repeating_notes.json", "utf8")
-    )
+  input_notes_data[1] = JSON.parse(
+    fs.readFileSync("./notes/one_measure_repeating_notes.json", "utf8")
   );
-  four_measure_repeating_notes.push(
-    JSON.parse(
-      fs.readFileSync("./notes/four_measure_repeating_notes.json", "utf8")
-    )
+  input_notes_data[2] = JSON.parse(
+    fs.readFileSync("./notes/one_measure_repeating_notes.json", "utf8")
   );
+  one_measure_repeating_notes = input_notes_data[0];
 };
-const select_time = () => {
+const select_time = (measure_repeating_time) => {
   let times_sum = 0;
   let random_song_num_4_first_note = Math.floor(
-    Math.random() * one_measure_repeating_notes.notes.length
+    Math.random() * input_notes_data[measure_repeating_time].notes.length
   );
   times.push(
-    one_measure_repeating_notes.notes[random_song_num_4_first_note][0].time[0]
+    input_notes_data[measure_repeating_time].notes[
+      random_song_num_4_first_note
+    ][0].time[0]
   ); //最初の音決定
-  while (times_sum < 2) {
+  let max_times_sum = null;
+  if (measure_repeating_time == 0) {
+    max_times_sum = 2;
+  } else if (measure_repeating_time == 1) {
+    max_times_sum = 4;
+  } else if (measure_repeating_time == 2) {
+    max_times_sum = 8;
+  }
+  while (times_sum < max_times_sum) {
     let random_song_num = Math.floor(
-      Math.random() * one_measure_repeating_notes.notes.length
+      Math.random() * input_notes_data[0].notes.length
     );
     let random_measure_num = Math.floor(Math.random() * 3);
     let random_note_num = Math.floor(
       Math.random() *
-        one_measure_repeating_notes.notes[random_song_num][random_measure_num]
-          .time.length
+        input_notes_data[measure_repeating_time].notes[random_song_num][
+          random_measure_num
+        ].time.length
     );
     while (random_note_num == 0) {
       random_note_num = Math.floor(
         Math.random() *
-          one_measure_repeating_notes.notes[random_song_num][random_measure_num]
-            .time.length
+          input_notes_data[measure_repeating_time].notes[random_song_num][
+            random_measure_num
+          ].time.length
       );
     }
     let selected_time = null;
     if (
-      one_measure_repeating_notes.notes[random_song_num][random_measure_num]
-        .time[random_note_num] !== 0
+      input_notes_data[measure_repeating_time].notes[random_song_num][
+        random_measure_num
+      ].time[random_note_num] !== 0
     ) {
       selected_time =
-        one_measure_repeating_notes.notes[random_song_num][random_measure_num]
-          .time[random_note_num] -
-        one_measure_repeating_notes.notes[random_song_num][random_measure_num]
-          .time[random_note_num - 1];
+        input_notes_data[measure_repeating_time].notes[random_song_num][
+          random_measure_num
+        ].time[random_note_num] -
+        input_notes_data[measure_repeating_time].notes[random_song_num][
+          random_measure_num
+        ].time[random_note_num - 1];
     }
     times_sum += selected_time;
-    if (times_sum + selected_time < 2) {
+    if (times_sum + selected_time < max_times_sum) {
       times.push(selected_time); //決定したtimeをtimesにpush
     }
   }
@@ -63,6 +81,7 @@ const select_time = () => {
 };
 const main = () => {
   input_notes();
-  select_time();
+  measure_repeating_time = 0;
+  select_time(measure_repeating_time);
 };
 main();
