387cf1a4b0da3f162435217083baf800f93699f0
kspace-trk <keigonakao.0825@gmail.com>
2021-09-19 14:07:42 +0900

    [add]export

---
+++ json2midi/export.js
@@ -0,0 +1,23 @@
+const { Midi } = require('@tonejs/midi')
+const fs  = require('fs')
+
+const filename = './test.json';
+
+function createMidiFromJson(filename) {
+        console.log(filename);
+        const json = JSON.parse(fs.readFileSync(filename, 'utf8'));
+        const midi2 = new Midi();
+        midi2.fromJSON(json);
+        fs.writeFileSync(filename + '.mid', Buffer.from(midi2.toArray()))
+        return;
+}
+
+function createMidiRaw(){
+        let midi = new Midi()
+        const track = midi.addTrack()
+        track.addNote({midi: 60, time: 0, duration: 0.2})
+        fs.writeFileSync( 'output.midi' , Buffer.from( midi.toArray()))
+}
+
+createMidiRaw();
+createMidiFromJson(filename);
